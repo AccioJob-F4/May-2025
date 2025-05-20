4d063095b612cd4d09c9edaedcb9ed4fa67b6360
RgnDunes <divyansh.singh@razorpay.com>
2025-05-20 08:14:41 +0530

    Refine JavaScript object manipulation examples by adding new use cases, enhancing explanations of deep vs shallow copy concepts, and improving overall structure for better clarity and organization.

---
+++ assignments/DOM Basics/Form Login/script.js
@@ -0,0 +1,11 @@
+function getFormvalue() {
+  event.preventDefault();
+  let fName = document.getElementById("fname").value.trim();
+  let lName = document.getElementById("lname").value.trim();
+
+  if (fName === "" || lName === "") {
+    alert("Please enter both first and last name.");
+    return;
+  }
+  alert(fName + " " + lName);
+}
---
+++ session 06/basics.js
@@ -0,0 +1,39 @@
+// Selecting DOM elements
+
+// getElementById --> Selects an element using its id attribute --> returns just one element
+
+// const greetBtn = document.getElementById("greet-btn");
+
+// getElementsByClassName --> Selects all elements with the given class name --> returns a list of elements (HTMLCollection)
+
+// const listItems = document.getElementsByClassName("greet-btn-class");
+
+// console.log(listItems);
+
+// getElementsByTagName --> Selects all elements with the given tag name --> returns a list of elements (HTMLCollection)
+
+// const listItems = document.getElementsByTagName("li");
+
+// console.log(listItems);
+
+// querySelector --> Selects the first element that matches the given CSS selector --> returns just one element
+
+// const greetBtnId = document.querySelector("#greet-btn");
+// const greetBtnClass = document.querySelector(".greet-btn-class");
+// const greetBtnTag = document.querySelector("li");
+
+// console.log(greetBtnId);
+// console.log(greetBtnClass);
+// console.log(greetBtnTag);
+
+// querySelectorAll --> Selects all elements that match the given CSS selector --> returns a list of elements (NodeList)
+
+// const greetBtnId = document.querySelectorAll("#greet-btn");
+// const greetBtnClass = document.querySelectorAll(".greet-btn-class");
+// const greetBtnTag = document.querySelectorAll("li");
+
+// console.log(greetBtnId);
+// console.log(greetBtnClass);
+// console.log(greetBtnTag);
+
+// ------------------------------------------------------------
