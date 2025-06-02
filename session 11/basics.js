41853a547f41037d9089c246ad9f7a44baa997bd
RgnDunes <divyansh.singh@razorpay.com>
2025-06-02 21:24:04 +0530

    Refactor fetch example in basics.js to include authorization header
    - Replaced commented-out fetch example with an active POST request to demonstrate data submission.
    - Added an Authorization header to the fetch request for enhanced security.
    - Updated comments to clarify the purpose of various sections, including async-await and role-based access control.

--- session 11/basics.js
+++ session 11/basics.js
@@ -24,45 +24,52 @@ let resp;
 //     console.log(error);
 //   });
 
-// fetch("https://jsonplaceholder.typicode.com/posts", {
-//   method: "POST",
-//   body: JSON.stringify({
-//     title: "My New Post Title",
-//     body: "This is the body of the post",
-//     userId: 42,
-//   }),
-// })
-//   .then((data) => {
-//     return data.json();
-//   })
-//   .then((response) => {
-//     console.log(response);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "My New Post Title",
+    body: "This is the body of the post",
+    userId: 42,
+  }),
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+})
+  .then((data) => {
+    return data.json();
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// RBAC --> Role Based Access Control
+// Private Protected Public Routes --> reactJS --> React Router DOM v5
+// useState --> reactJS --> React Hooks
+// State Management --> Redux, Flux, Zustand, Context API
 
 // async await
+// const getPostByID = async (postId) => {
+//   const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
 
-const getPostByID = async (postId) => {
-  const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+//   //   .then .catch way
+//   //   fetch(url)
+//   //     .then((data) => data.json())
+//   //     .then((response) => {})
+//   //     .catch((error) => {
+//   //       console.log(error);
+//   //     });
 
-  //   .then .catch way
-  //   fetch(url)
-  //     .then((data) => data.json())
-  //     .then((response) => {})
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
+//   //   async-await way
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-  //   async-await way
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-getPostByID(1);
+// getPostByID(1);
